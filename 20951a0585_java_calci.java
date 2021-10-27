b7843927f35464d2d4074bbc4a150395dd02502e
Manoj Motapalukula <89991377+manojmotapalukula@users.noreply.github.com>
2021-10-27 11:12:10 +0530

    update successfully

--- 20951a0585_java_calci.java
+++ 20951a0585_java_calci.java
@@ -1,4 +1,63 @@
-run:
+
+/*
+ simple calculator
+ */
+package javaapplication27;
+import java.util.*;
+
+public class JavaApplication27 {
+     static void funSum(int a, int b){
+        System.out.println("sum is " + (a+b));
+     }
+     static void funSub(int a, int b){
+        System.out.println("sub is " + (a-b));
+     }
+     static void funMul(int a, int b){
+        System.out.println("mul is " + (a*b));
+     }
+      static void funDiv(int a, int b){
+        System.out.println("Div is " + (a/b));
+      }
+
+  
+    public static void main(String[] args) {
+        int choice;
+        System.out.println("menu\n1.add\n2.sub\n3.mul\n4.div ");
+        
+        int i=1;
+        while (i>0 ) {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("enter your choice: ");
+            choice = sc.nextInt(); 
+            int x , y;
+            System.out.println("enter x ");
+            x = sc.nextInt();
+            System.out.println("enter y ");
+            y = sc.nextInt();
+            if (choice==1){
+                 funSum(x,y);
+        }
+            if (choice==2){
+                 funSub(x,y);
+        }
+            if (choice==3){
+                 funMul(x,y);
+        }
+            if (choice==4){
+                 funDiv(x,y);
+        }
+            if (choice >4 | choice==0){
+                System.out.println("enter a valid choice ");
+                break;
+            }
+            
+  
+      
+}
+    }
+}
+
+Input/Output:
 menu
 1.add
 2.sub
@@ -25,4 +84,3 @@ enter x
 enter y 
 5
 enter a valid choice 
-BUILD SUCCESSFUL (total time: 19 seconds)
