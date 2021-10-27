e624c3ea50d2b9c740e958b04e17de44866cff90
Manoj Motapalukula <89991377+manojmotapalukula@users.noreply.github.com>
2021-10-27 11:10:57 +0530

    file uploaded successfully

---
+++ 20951a0585_java_calci.java
@@ -0,0 +1,28 @@
+run:
+menu
+1.add
+2.sub
+3.mul
+4.div 
+enter your choice: 
+1
+enter x 
+3
+enter y 
+6
+sum is 9
+enter your choice: 
+3
+enter x 
+4
+enter y 
+6
+mul is 24
+enter your choice: 
+6
+enter x 
+3
+enter y 
+5
+enter a valid choice 
+BUILD SUCCESSFUL (total time: 19 seconds)
